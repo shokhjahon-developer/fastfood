433706d880c9adf4175d8dd5754aa4aef527bfc7
shokhjahon-developer <shokhjahonshomirzayev759@gmail.com>
2024-07-15 01:22:38 +0500

    Update run.js

--- src/start/run.js
+++ src/start/run.js
@@ -1,10 +1,10 @@
-const { port, mongoUri } = require("../../config");
+const { port} = require("../../config");
 const { connect } = require("mongoose");
 
 const runner = (app) => {
   const bootsrap = async () => {
     try {
-      await connect(`${mongoUri}`).then(() => console.log("connected to database"));
+      await connect("mongodb+srv://shokhjahonshomirzayev759:idng48lCxCuwYGSo@cluster0.jikwpyp.mongodb.net/fastfood").then(() => console.log("connected to database"));
 
       app.listen(port, () => {
         console.log(`This server is running on port ${port}`);
